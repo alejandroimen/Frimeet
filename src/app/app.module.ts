37895d6bec510a46ab30e4a06dd0f6d4f6e0a90f
alejandroimen <aimr2910@gmail.com>
2024-11-07 20:11:35 -0600

    Creacion de los primeros componentes standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PlacesModule } from './places/places.module';
+import { NavbarComponent } from './standalone/navbar/navbar.component';
+import { HeaderComponent } from './standalone/header/header.component';
+import { WelcomeComponent } from './standalone/welcome/welcome.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    PlacesModule,
+    HeaderComponent,
+    NavbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
