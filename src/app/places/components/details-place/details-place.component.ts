dc221e40474419a527a0769fcfaf7a9bb8f90769
JosephAntony37900 <Joseantoniopintoaguilar@gmail.com>
2024-11-11 19:41:42 -0600

    Ver y eliminar lugares realizados con exito y estilos decentes

--- src/app/places/components/details-place/details-place.component.ts
+++ src/app/places/components/details-place/details-place.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PlaceService } from '../../services/place.service';
 import { Iplace } from '../../interfaces/iplace';
 
@@ -10,8 +10,9 @@ import { Iplace } from '../../interfaces/iplace';
 })
 export class DetailsPlaceComponent implements OnInit {
   place: Iplace | undefined;
+  deleteModal: any;
 
-  constructor(private placeService: PlaceService, private route: ActivatedRoute) {}
+  constructor(private placeService: PlaceService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     const placeId = this.route.snapshot.paramMap.get('id');
@@ -20,6 +21,28 @@ export class DetailsPlaceComponent implements OnInit {
         this.place = data;
       }, error => {
         console.error('Error al obtener los detalles del lugar:', error);
+      });
+    }
+  }
+
+  openModal(): void {
+    const modalElement = document.getElementById('deleteModal');
+    if (modalElement) {
+      this.deleteModal = new (window as any).bootstrap.Modal(modalElement);
+      this.deleteModal.show();
+    }
+  }
+
+  deletePlace(): void {
+    if (this.place && this.place._id) {
+      this.placeService.deletePlace(this.place._id).subscribe(response => {
+        console.log('Lugar eliminado:', response);
+        if (this.deleteModal) {
+          this.deleteModal.hide();
+        }
+        this.router.navigate(['/info-place']);
+      }, error => {
+        console.error('Error al eliminar el lugar:', error);
       });
     }
   }
